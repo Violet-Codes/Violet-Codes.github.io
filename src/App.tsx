5dbda616e2fd71c763b85793e289cc54860f0796
Violet-Codes <125933964+Violet-Codes@users.noreply.github.com>
2023-02-22 23:24:50 -0600

    simple bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,36 @@
 import React from 'react';
-import {Counter} from './Counter';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Nav from 'react-bootstrap/Nav';
 
-function App() {
+export const App: React.FC<{}> = (props: {}) => {
   return (
-    <div className="App">
-      <header className="App-header">
+    <>
+      <Nav defaultActiveKey="link-0"  className="justify-content-center text-bg-dark" as="ul">
+        <Nav.Item as="li">
+          <Nav.Link eventKey="link-0">Foo</Nav.Link>
+        </Nav.Item>
+        <Nav.Item as="li">
+          <Nav.Link eventKey="link-1">Bar</Nav.Link>
+        </Nav.Item>
+        <Nav.Item as="li">
+          <Nav.Link eventKey="link-2">Qux</Nav.Link>
+        </Nav.Item>
+      </Nav>
+      <header className="p-4 mb-4 border-3 border-bottom">
         <h1>
           Hi there! 👋
         </h1>
       </header>
       <main>
-        <p>
-          I am a self-taught developer with an affinity for mathematics and its applications in development.
-          I program mainly in Haskell, Rust, and Python.
+        <div className="ms-4">
+          I am a self-taught developer with an affinity for mathematics and its applications in development.<br/>
+          I program mainly in Haskell, Rust, and Python.<br/>
           I excel at learning and applying design patterns such as Composition, Dependency Injection and Mutability Safety.
-        </p>
-        <Counter initial={0}/>
+        </div>
       </main>
-    </div>
+    </>
   );
 }
 
